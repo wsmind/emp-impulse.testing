3007a0524703613dd742ecf1d2044249d5b9e24b
remi.papillie <remi.papillie@localhost>
2010-09-18 23:27:11 +0200

    doxygen documentation for the PEEL library

--- test/peel/peel.hpp
+++ test/peel/peel.hpp
@@ -20,12 +20,30 @@
  *                                                                             *
  ******************************************************************************/
 
+/**
+ * \file
+ * \brief Problem Elusive Examination Library
+ * 
+ * This header is a lightweight, stand-alone test library. It is used throughout
+ * EMP projects, to implement automated unit tests.
+ */
+
 #ifndef __PEEL_HPP__
 #define __PEEL_HPP__
 
 #include <iostream>
 #include <cmath>
 
+/**
+ * \def CHECK(expression)
+ * \brief Assert the given \a expression and print the result to stdout or stderr
+ * 
+ * The behavior of this macro depends upon the result of the evaluation of the
+ * given expression. If it evaluates to true, the string "[OK]" followed by
+ * the expression is output on stdout. On the other hand, when the result is
+ * false, an error is output on stderr with the format
+ * "[FAILED] expression (file:line)".
+ */
 #define CHECK(expression) \
 	{ \
 		if (expression) \
@@ -34,6 +52,7 @@
 			std::cerr << "[FAILED] " << #expression << " (" << __FILE__ << ":" << __LINE__ << ")" << std::endl; \
 	}
 
+//! Accepted float error for comparisons
 #define FLOAT_EPSILON 0.001f
 
 #define FLOAT_EQUAL(f1, f2) \
