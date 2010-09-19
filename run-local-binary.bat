a50785f009b0d8058f7a275cdb756f1ff2b114d2
remi.papillie <remi.papillie@localhost>
2010-09-19 18:18:57 +0200

    added local binary run scripts

---
+++ run-local-binary.bat
@@ -0,0 +1,4 @@
+rem Run a binary using local libraries (the ones in the lib/ directory).
+rem Without this script, the system cannot find these dynamic libraries.
+set PATH=lib;lib\thirdparty;%PATH%
+%1 %2 %3 %4 %5 %6 %7 %8 %9
