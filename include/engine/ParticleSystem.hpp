dad481b04d022e4597b395cafb880bca320f2f3e
davy.leggieri <davy.leggieri@gmail.com>
2010-10-01 00:07:06 +0100

    Starting ParticleSystem class

---
+++ include/engine/ParticleSystem.hpp
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ *                                                                             *
+ * This file is part of the EMP Impulse project                                *
+ *                                                                             *
+ * Copyright (C) 2010 ElectroMagnetic Potatoes (EMP)                           *
+ * See the AUTHORS file for more information.                                  *
+ *                                                                             *
+ * This program is free software: you can redistribute it and/or modify        *
+ * it under the terms of the GNU General Public License as published by        *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                             *
+ ******************************************************************************/
+
+#ifndef __PARTICLESYSTEM_HPP__
+#define __PARTICLESYSTEM_HPP__
+
+#include <common.hpp>
+#include <math/Vec2.hpp>
+#include <engine/Particle.hpp>
+#include <engine/ParticleBin.hpp>
+#include <SFML/Graphics.hpp>
+
+namespace engine {
+	
+	/**
+	 * \class ParticleSystem
+	 * \brief UI particle effects
+	 * \author Davy LEGGIERI
+	 *
+	 */
+	class ParticleSystem
+	{
+	public:		
+		/**
+		 * \brief Construct a particleSystem
+		 */
+		ParticleSystem();
+		
+		/**
+		 * \brief Know if particleSystem is active (try producing particle)
+		 */
+		bool isActive();
+		
+		/**
+		 * \brief Active/Inactive particleSystem
+		 * \param active true to activate the particleSystem, false to inactivate it.
+		 */
+		void setActive(bool active);
+		
+		/**
+		 * \brief Update the particleSystem
+		 * \param dt delta time before last update call
+		 * \param forces forces to apply on each particle
+		 */
+		void update(float dt, math::Vec2 forces);
+		
+		/**
+		 * \brief Draw the particleSystem in the windows
+		 */
+		void draw(sf::RenderWindow& window) const;
+	p
+	rivate:
+		ParticleBin bin;
+	};
+	
+} // engine namespace
+
+#endif // __PARTICLESYSTEM_HPP__
+
